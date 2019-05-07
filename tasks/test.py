7d21dcda036e9cb2eda5d80f0417fe147ffc0e62
Maxime Mouial <maxime.mouial@gmail.com>
2019-05-07 14:27:48 -0600

    Remove temporary changes to the gitlab pipeline
    We temporary allowed failure on Suse and Windows when developping six.

--- tasks/test.py
+++ tasks/test.py
@@ -78,7 +78,7 @@ def test(ctx, targets=None, coverage=False, build_include=None, build_exclude=No
     lint(ctx, targets=tool_targets)
     lint_licenses(ctx)
     print("--- Vetting:")
-    vet(ctx, targets=tool_targets, six_root=six_root)
+    vet(ctx, targets=tool_targets, six_root=six_root, build_tags=build_tags)
     print("--- Misspelling:")
     misspell(ctx, targets=tool_targets)
     print("--- ineffassigning:")
